438419604b755ccfec986134cb5e45511b0535d5
ElevenNotes <github@11notes.ch>
2026-03-30 14:52:13 +0200

    feat: switch to go-eleven

--- build/go/entrypoint/go.mod
+++ build/go/entrypoint/go.mod
@@ -1,5 +1,5 @@
 module github.com/11notes/docker-radicale/entrypoint
 go 1.25.0
 require (
-  github.com/11notes/go v1.1.22
+  github.com/11notes/go-eleven v0.3.0
 )
--- build/go/entrypoint/main.go
+++ build/go/entrypoint/main.go
@@ -2,15 +2,10 @@ package main
 
 import (
 	"os"
-	"syscall"
 	"encoding/base64"
 	"strings"
 
-	"github.com/11notes/go"
-)
-
-var (
-	Eleven eleven.New = eleven.New{}
+	"github.com/11notes/go-eleven"
 )
 
 const ENV_LDAP_CALENDAR_GROUPS string = "RADICALE_LDAP_CALENDAR_GROUPS"
@@ -25,16 +20,16 @@ func LDAPCreateCalendars(){
 			root := ROOT_LDAP_GROUPS + "/" + groupNameBase64 + "/.Radicale.cache/sync-token"
 			if _, err := os.Stat(root); os.IsNotExist(err){
 				if err := os.MkdirAll(root, os.ModePerm); err != nil {
-					Eleven.Log("ERR", "could not create LDAP group calendar %s", group)
+					eleven.Log("ERR", "could not create LDAP group calendar %s", group)
 				}else{
-					if err := Eleven.Util.WriteFile(ROOT_LDAP_GROUPS + "/" + groupNameBase64 + "/.Radicale.props", `{"C:calendar-description": "`+group+`", "C:supported-calendar-component-set": "VEVENT,VJOURNAL,VTODO", "D:displayname": "`+group+`", "ICAL:calendar-color": "#0692c9ff", "ICAL:calendar-order": "2", "tag": "VCALENDAR"}`); err != nil {
-						Eleven.Log("ERR", "could not create LDAP group calendar %s property object", group)
+					if err := eleven.Util.WriteFile(ROOT_LDAP_GROUPS + "/" + groupNameBase64 + "/.Radicale.props", `{"C:calendar-description": "`+group+`", "C:supported-calendar-component-set": "VEVENT,VJOURNAL,VTODO", "D:displayname": "`+group+`", "ICAL:calendar-color": "#0692c9ff", "ICAL:calendar-order": "2", "tag": "VCALENDAR"}`); err != nil {
+						eleven.Log("ERR", "could not create LDAP group calendar %s property object", group)
 					}else{
-						Eleven.Log("INF", "created LDAP group calendar %s", group)
+						eleven.Log("INF", "created LDAP group calendar %s", group)
 					}
 				}
 			}else{
-				Eleven.Log("INF", "LDAP group calendar %s already exists", group)
+				eleven.Log("INF", "LDAP group calendar %s already exists", group)
 			}
 		}
 	}
@@ -48,16 +43,16 @@ func LDAPCreateAddressBooks(){
 			root := ROOT_LDAP_GROUPS + "/" + groupNameBase64 + "/.Radicale.cache/sync-token"
 			if _, err := os.Stat(root); os.IsNotExist(err){
 				if err := os.MkdirAll(root, os.ModePerm); err != nil {
-					Eleven.Log("ERR", "could not create LDAP group address book %s", group)
+					eleven.Log("ERR", "could not create LDAP group address book %s", group)
 				}else{
-					if err := Eleven.Util.WriteFile(ROOT_LDAP_GROUPS + "/" + groupNameBase64 + "/.Radicale.props", `{"D:displayname": "`+group+`", "tag": "VADDRESSBOOK", "{http://inf-it.com/ns/ab/}addressbook-color": "#cfaedbff"}`); err != nil {
-						Eleven.Log("ERR", "could not create LDAP group address book %s property object", group)
+					if err := eleven.Util.WriteFile(ROOT_LDAP_GROUPS + "/" + groupNameBase64 + "/.Radicale.props", `{"D:displayname": "`+group+`", "tag": "VADDRESSBOOK", "{http://inf-it.com/ns/ab/}addressbook-color": "#cfaedbff"}`); err != nil {
+						eleven.Log("ERR", "could not create LDAP group address book %s property object", group)
 					}else{
-						Eleven.Log("INF", "created LDAP group address book %s", group)
+						eleven.Log("INF", "created LDAP group address book %s", group)
 					}
 				}
 			}else{
-				Eleven.Log("INF", "LDAP group address book %s already exists", group)
+				eleven.Log("INF", "LDAP group address book %s already exists", group)
 			}
 		}
 	}
@@ -67,12 +62,9 @@ func main() {
 	LDAPCreateCalendars()
 	LDAPCreateAddressBooks()
 
-	_ = Eleven.Container.EnvToFile("RADICALE_CONFIG", "/radicale/etc/default.conf")
-	_ = Eleven.Container.EnvToFile("RADICALE_RIGHTS", "/radicale/etc/rights")
-	_ = Eleven.Container.EnvToFile("RADICALE_USERS", "/radicale/etc/users")
+	_ = eleven.Container.EnvToFile("RADICALE_CONFIG", "/radicale/etc/default.conf")
+	_ = eleven.Container.EnvToFile("RADICALE_RIGHTS", "/radicale/etc/rights")
+	_ = eleven.Container.EnvToFile("RADICALE_USERS", "/radicale/etc/users")
 
-	Eleven.Log("START", "")
-	if err := syscall.Exec("/usr/local/bin/python", []string{"python", "-m", "radicale", "--config", "/radicale/etc/default.conf"}, os.Environ()); err != nil {
-		os.Exit(1)
-	}
+	eleven.Container.Run("/usr/local/bin", "python", []string{"-m", "radicale", "--config", "/radicale/etc/default.conf"})
 }
